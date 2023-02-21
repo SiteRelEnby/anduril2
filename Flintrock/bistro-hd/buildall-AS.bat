0bc3acc50f1bce931bcbed8b743149f1dc1eaf18
SiteRelEnby <SiteRelEnby@localhost>
2023-02-21 13:28:03 -0600

    removed other fw

--- Flintrock/bistro-hd/buildall-AS.bat
+++
@@ -1,3 +0,0 @@
-make clean GCC_DIR="C:\WinAVR-20100110\bin" INCLUDES="C:\WinAVR-20100110\avr\include\avr"
-make all GCC_DIR="C:\WinAVR-20100110\bin" INCLUDES="C:\WinAVR-20100110\avr\include\avr"
-pause
