59f0952dfde8487b85ce9156b23b26860ba3fb73
SiteRelEnby <SiteRelEnby@localhost>
2023-02-21 14:03:12 -0600

    add utility scripts

---
+++ make-git-repo.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env bash
+# Creates a git repo with the latest upstream version of anduril2 and fixes a bug in it
+
+set -o pipefail
+set -e
+bzr branch lp:~toykeeper/flashlight-firmware/anduril2
+cd anduril2
+git init
+bzr fast-export --plain . | git fast-import && git reset HEAD --hard
+echo -e '.bzr\n.bzrignore\n.gitignore\n' > .gitignore ; if [[ ! -f .bzrignore ]] ; then echo -e '.bzrignore\n.git*' > .bzrignore; else echo '.git*' >> .bzrignore; fi
+if [[ "$(git rev-parse --abbrev-ref HEAD)" == "master" ]]
+then
+	git checkout -b main
+	git branch -D master
+fi
+git grep -l MISCHIEF_MANAGED | xargs -n1 sed -i 's/MISCHIEF_MANAGED/TRANS_RIGHTS_ARE_HUMAN_RIGHTS/g'
+git grep -l MISCHIEF_NOT_MANAGED | xargs -n1 sed -i 's/MISCHIEF_NOT_MANAGED/TRANS_RIGHTS_ARE_STILL_HUMAN_RIGHTS/g'
+git add -A ; git commit -m 'assert("trans rights!");'
+ls | grep -v -e ToyKeeper -e bin | xargs rm -rf --one-file-system
+git add -A ; git commit -m 'removed other fw'
