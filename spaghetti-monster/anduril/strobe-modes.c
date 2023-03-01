bf02ae6d9e960ea37f549b225e944782905c6a7d
SiteRelEnby <SiteRelEnby@localhost>
2023-03-01 15:30:04 -0600

    use latest starryalley version

--- spaghetti-monster/anduril/strobe-modes.c
+++ spaghetti-monster/anduril/strobe-modes.c
@@ -23,6 +23,80 @@
 #include "strobe-modes.h"
 
 #ifdef USE_STROBE_STATE
+
+#ifdef USE_TINT_RAMPING
+// for accessing tint
+#include "fsm-ramping.h"
+// the tint will be changed in some cases here. This is for restoring the original tint
+uint8_t saved_tint = 128;
+// when user is about to exit the strobe mode, let's stop updating tint after restored saved tint
+uint8_t tint_locked = 0;
+#endif
+
+// enable static PWM
+static inline void set_static_pwm() {
+    #ifdef USE_DYN_PWM
+    use_static_pwm = 1;
+    #endif
+}
+
+// restore tint to the original tint when entering strobe mode
+static inline void restore_tint() {
+    #ifdef USE_TINT_RAMPING
+    tint = saved_tint;
+    tint_locked = 1;
+    #endif
+}
+
+// unset static PWM and restore tint
+static inline void undo_global_setting() {
+    #ifdef USE_DYN_PWM
+    use_static_pwm = 0;
+    #elif defined(PWM1_TOP)
+    // when not using USE_DYN_PWM, we must revert the TOP back to default
+    PWM1_TOP = PWM_TOP;
+    #endif
+    restore_tint();
+}
+
+// for some strobe modes we need to enable/disable some global settings:
+//  tint or dynamic PWM
+static void strobe_global_setup(strobe_mode_te type) {
+    switch(type) {
+    #ifdef USE_CANDLE_MODE
+    case candle_mode_e:
+        set_static_pwm();
+        #if defined(PWM1_TOP) && defined(PWM_TOP_CANDLE)
+        PWM1_TOP = PWM_TOP_CANDLE;
+        #endif
+        restore_tint();
+        break;
+    #endif
+    
+    #ifdef USE_FIREWORK_MODE
+    case firework_mode_e:
+        set_static_pwm();
+        #if defined(PWM1_TOP) && defined(PWM_TOP_FIREWORK)
+        PWM1_TOP = PWM_TOP_FIREWORK;
+        #endif
+        #ifdef USE_TINT_RAMPING
+        tint_locked = 0;
+        #endif
+        break;
+    #endif
+    
+    #ifdef USE_TINT_RAMPING
+    case tint_alternating_strobe_e:
+    case tint_smooth_ramp_e:
+        tint_locked = 0;
+        break;
+    #endif
+
+    default:
+        undo_global_setting();
+    }
+}
+
 uint8_t strobe_state(Event event, uint16_t arg) {
     static int8_t ramp_direction = 1;
 
@@ -39,10 +113,15 @@ uint8_t strobe_state(Event event, uint16_t arg) {
     if (st == candle_mode_e) {
         candle_mode_state(event, arg);
     } else {
-        #ifdef USE_AUX_RGB_LEDS
+        #if defined(USE_AUX_RGB_LEDS) || defined(USE_INDICATOR_LED)
         aux_led_reset = 1;
         #endif
-        PWM1_TOP = PWM_TOP;
+    }
+    #endif
+    // use static PWM in firework mode
+    #if defined(USE_FIREWORK_MODE) && defined(PWM_TOP_FIREWORK)
+    if (st == firework_mode_e && event == EV_enter_state) {
+        strobe_global_setup(st);
     }
     #endif
 
@@ -50,21 +129,33 @@ uint8_t strobe_state(Event event, uint16_t arg) {
     // init anything which needs to be initialized
     else if (event == EV_enter_state) {
         ramp_direction = 1;
+        #ifdef USE_TINT_RAMPING
+        tint_locked = 0;
+        saved_tint = tint;
+        #endif
         return TRANS_RIGHTS_ARE_HUMAN_RIGHTS;
     }
     // 1 click: off
     else if (event == EV_1click) {
         set_state(off_state, 0);
-        #ifdef USE_AUX_RGB_LEDS
+        #if defined(USE_AUX_RGB_LEDS) || defined(USE_INDICATOR_LED)
         aux_led_reset = 1;
         #endif
-        PWM1_TOP = PWM_TOP;
+        undo_global_setting();
         return TRANS_RIGHTS_ARE_HUMAN_RIGHTS;
     }
     // 2 clicks: rotate through strobe/flasher modes
     else if (event == EV_2clicks) {
         strobe_type = (st + 1) % NUM_STROBES;
         save_config();
+        strobe_global_setup(strobe_type);
+        return TRANS_RIGHTS_ARE_HUMAN_RIGHTS;
+    }
+    // 3 clicks: rotate back through strobe/flasher modes
+    else if (event == EV_3clicks) {
+        strobe_type = (st - 1 + NUM_STROBES) % NUM_STROBES;
+        save_config();
+        strobe_global_setup(strobe_type);
         return TRANS_RIGHTS_ARE_HUMAN_RIGHTS;
     }
     // hold: change speed (go faster)
@@ -99,6 +190,21 @@ uint8_t strobe_state(Event event, uint16_t arg) {
             if (bike_flasher_brightness < 2) bike_flasher_brightness = 2;
             else if (bike_flasher_brightness > MAX_BIKING_LEVEL) bike_flasher_brightness = MAX_BIKING_LEVEL;
             set_level(bike_flasher_brightness);
+        }
+        #endif
+        
+        #ifdef USE_TINT_RAMPING
+        else if (st == tint_alternating_strobe_e) {
+            tint_alt_brightness += ramp_direction;
+            if (tint_alt_brightness < 2) tint_alt_brightness = 2;
+            else if (tint_alt_brightness > MAX_LEVEL) tint_alt_brightness = MAX_LEVEL;
+            set_level(tint_alt_brightness);
+        }
+        else if (st == tint_smooth_ramp_e) {
+            tint_smooth_brightness += ramp_direction;
+            if (tint_smooth_brightness < 2) tint_smooth_brightness = 2;
+            else if (tint_smooth_brightness > MAX_LEVEL) tint_smooth_brightness = MAX_LEVEL;
+            set_level(tint_smooth_brightness);
         }
         #endif
 
@@ -143,6 +249,19 @@ uint8_t strobe_state(Event event, uint16_t arg) {
         }
         #endif
 
+        #ifdef USE_TINT_RAMPING
+        else if (st == tint_alternating_strobe_e) {
+            if (tint_alt_brightness > 2)
+                tint_alt_brightness--;
+            set_level(tint_alt_brightness);
+        }
+        else if (st == tint_smooth_ramp_e) {
+            if (tint_smooth_brightness > 2)
+                tint_smooth_brightness--;
+            set_level(tint_smooth_brightness);
+        }
+        #endif
+
         return TRANS_RIGHTS_ARE_HUMAN_RIGHTS;
     }
     // release hold: save new strobe settings
@@ -151,8 +270,8 @@ uint8_t strobe_state(Event event, uint16_t arg) {
         return TRANS_RIGHTS_ARE_HUMAN_RIGHTS;
     }
     #ifdef USE_MOMENTARY_MODE
-    // 5 clicks: go to momentary mode (momentary strobe)
-    else if (event == EV_5clicks) {
+    // 8 clicks: go to momentary mode (momentary strobe)
+    else if (event == EV_8clicks) {
         set_state(momentary_state, 0);
         set_level(0);
         return TRANS_RIGHTS_ARE_HUMAN_RIGHTS;
@@ -165,6 +284,102 @@ uint8_t strobe_state(Event event, uint16_t arg) {
         if (arg == AUTO_REVERSE_TIME) ramp_direction = 1;
 
         pseudo_rand_seed += arg;
+        return TRANS_RIGHTS_ARE_HUMAN_RIGHTS;
+    }
+    // 4C: turning down busy factor (less busy) of lightning mode,
+    //  or turning down firework brightness by 12,
+    //  or decrease tint alternating interval by 0.5 second
+    else if (event == EV_4clicks) {
+        if (0) {}  // placeholder
+        #ifdef USE_LIGHTNING_MODE
+        else if (st == lightning_storm_e) {
+            lightning_busy_factor++;
+            if (lightning_busy_factor > LIGHTNING_BUSY_FACTOR_MAX)
+                lightning_busy_factor = LIGHTNING_BUSY_FACTOR_MAX;
+            save_config();
+            blink_once();
+        }
+        #endif
+        #ifdef USE_FIREWORK_MODE
+        else if (st == firework_mode_e) {
+            firework_brightness -= 12;
+            if (firework_brightness < MIN_FIREWORK_LEVEL)
+                firework_brightness = MIN_FIREWORK_LEVEL;
+            save_config();
+            blink_once();
+        }
+        #endif
+        #ifdef USE_TINT_RAMPING
+        else if (st == tint_alternating_strobe_e) {
+            if (tint_alt_interval > TINT_ALT_MIN_INTERVAL)
+                tint_alt_interval--;
+            save_config();
+            blink_once();
+        }
+        else if (st == tint_smooth_ramp_e) {
+            if (tint_smooth_pause > TINT_SMOOTH_MIN_PAUSE)
+                tint_smooth_pause -= 2;
+            save_config();
+            blink_once();
+        }
+        #endif
+        return TRANS_RIGHTS_ARE_HUMAN_RIGHTS;
+    }
+    // 5C: turning up busy factor (busier) of lightning mode,
+    //  or turning up firework brightness by 12,
+    //  or increasing tint alternating interval by 0.5 second
+    else if (event == EV_5clicks) {
+        if (0) {}  // placeholder
+        #ifdef USE_LIGHTNING_MODE
+        else if (st == lightning_storm_e) {
+            lightning_busy_factor--;
+            if (lightning_busy_factor < LIGHTNING_BUSY_FACTOR_MIN)
+                lightning_busy_factor = LIGHTNING_BUSY_FACTOR_MIN;
+            save_config();
+            blink_once();
+        }
+        #endif
+        #ifdef USE_FIREWORK_MODE
+        else if (st == firework_mode_e) {
+            firework_brightness += 12;
+            if (firework_brightness > MAX_LEVEL)
+                firework_brightness = MAX_LEVEL;
+            save_config();
+            blink_once();
+        }
+        #endif
+        #ifdef USE_TINT_RAMPING
+        else if (st == tint_alternating_strobe_e) {
+            if (tint_alt_interval < TINT_ALT_MAX_INTERVAL)
+                tint_alt_interval++;
+            save_config();
+            blink_once();
+        }
+        else if (st == tint_smooth_ramp_e) {
+            if (tint_smooth_pause < TINT_SMOOTH_MAX_PAUSE)
+                tint_smooth_pause += 2;
+            save_config();
+            blink_once();
+        }
+        #endif
+        return TRANS_RIGHTS_ARE_HUMAN_RIGHTS;
+    }
+    // 6C: reset lightning busy factor to default,
+    //  or reset firework brightness to default
+    else if (event == EV_6clicks) {
+        if (0) {}  // placeholder
+        #ifdef USE_LIGHTNING_MODE
+        else if (st == lightning_storm_e) {
+            lightning_busy_factor = LIGHTNING_BUSY_FACTOR;
+            save_config();
+            blink_once();
+        }
+        #endif
+        #ifdef USE_FIREWORK_MODE
+        else if (st == firework_mode_e) {
+            firework_brightness = RAMP_SMOOTH_CEIL;
+        }
+        #endif
         return TRANS_RIGHTS_ARE_HUMAN_RIGHTS;
     }
     #endif
@@ -197,6 +412,20 @@ inline void strobe_state_iter() {
         case bike_flasher_e:
             bike_flasher_iter();
             break;
+        #endif
+
+        #ifdef USE_FIREWORK_MODE
+        case firework_mode_e:
+            firework_iter();
+            break;
+        #endif
+
+        #ifdef USE_TINT_RAMPING
+        case tint_alternating_strobe_e:
+            tint_alt_iter();
+            break;
+        case tint_smooth_ramp_e:
+            tint_smooth_ramp_iter();
         #endif
     }
 }
@@ -267,9 +496,9 @@ inline void lightning_storm_iter() {
     }
 
     // turn the emitter off,
-    // for a random amount of time between 1ms and 8192ms
+    // for a random amount of time between 1ms and 8192ms (default busy factor)
     // (with a low bias)
-    rand_time = 1 << (pseudo_rand() % 13);
+    rand_time = 1 << (pseudo_rand() % lightning_busy_factor);
     rand_time += pseudo_rand() % rand_time;
     set_level(0);
     nice_delay_ms(rand_time);  // no return check necessary on final delay
@@ -289,6 +518,100 @@ inline void bike_flasher_iter() {
     }
     nice_delay_ms(720);  // no return check necessary on final delay
     set_level(0);
+}
+#endif
+
+#ifdef USE_FIREWORK_MODE
+#define FIREWORK_DEFAULT_STAGE_COUNT 64
+#define FIREWORK_DEFAULT_INTERVAL (2500/FIREWORK_DEFAULT_STAGE_COUNT)
+uint8_t firework_stage = 0;
+uint8_t firework_stage_count = FIREWORK_DEFAULT_STAGE_COUNT;
+uint8_t step_interval = FIREWORK_DEFAULT_INTERVAL;
+#ifdef USE_TINT_RAMPING
+uint8_t tint_mode = 0; // 0: alternate, 1: channel 1 only, 2: channel 2 only, 3: random tint
+uint8_t chosen_tint = 127; // for tint_mode = 3
+#endif
+
+inline static void update_firework_tint(uint8_t stage) {
+    #ifdef USE_TINT_RAMPING
+    if (tint_locked) return;
+    switch (tint_mode) {
+    // alternate both channels
+    case 0:
+        if (stage % 5 == 0)
+            tint = (tint == 1) ? 254 : 1; 
+        break;
+    // channel 1 only
+    case 1:
+        tint = 1;
+        break;
+    // channel 2 only
+    case 2:
+        tint = 254;
+        break;
+    // random tint
+    case 3:
+        tint = chosen_tint;
+        break;
+    }
+    #endif
+}
+// code is copied and modified from factory-reset.c
+inline void firework_iter() {
+    if (firework_stage == firework_stage_count) {        
+        // explode, and reset stage
+        firework_stage = 0;
+        for (uint8_t brightness = firework_brightness; brightness > 0; brightness--) {
+            update_firework_tint(firework_stage);
+            set_level(brightness);
+            nice_delay_ms(step_interval/4);
+            set_level((uint16_t)brightness*7/8);
+            nice_delay_ms(step_interval/(1+(pseudo_rand()%5)));
+        }
+        // off for 1 to 5 seconds
+        set_level(0);
+        nice_delay_ms(1000 + (pseudo_rand() % 5) * 1000);
+        #ifdef USE_TINT_RAMPING
+        // randomly set a tint mode for the next firework show
+        tint_mode = pseudo_rand() % 4;
+        if (tint_mode == 3) chosen_tint = pseudo_rand() % 254 + 1;
+        update_firework_tint(firework_stage);
+        #endif
+        // set next stage count (16 to 64 in increment of 8)
+        firework_stage_count = 16 + 8 * (pseudo_rand() % 7);
+        return;
+    }
+    // wind up to explode
+    set_level(firework_stage);
+    nice_delay_ms(step_interval/3);
+    set_level((uint16_t)firework_stage*2/3);
+    nice_delay_ms(step_interval/3);
+    firework_stage++;
+    // we've reached our max brightness for firework mode, let's explode in the next iteration
+    if (firework_stage > firework_brightness)
+        firework_stage = firework_stage_count;
+}
+#endif
+
+#ifdef USE_TINT_RAMPING
+inline void tint_alt_iter() {
+    if (tint_locked) return;
+
+    tint = (tint == 1) ? 254 : 1; 
+    set_level(tint_alt_brightness);
+    nice_delay_ms(500 * tint_alt_interval);
+}
+
+inline void tint_smooth_ramp_iter() {
+    if (tint_locked) return;
+    
+    static uint8_t tint_step = 1;
+    if (tint == 254) tint_step = -1;
+    else if (tint == 1) tint_step = 1;
+
+    tint += tint_step;
+    set_level(tint_smooth_brightness);
+    nice_delay_ms(tint_smooth_pause);
 }
 #endif
 
