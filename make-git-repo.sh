10a7e306897ad4e9ddcf259636fc2ebae3503178
SiteRelEnby <SiteRelEnby@localhost>
2023-03-05 10:49:21 -0600

    fix removal of unnecessary paths

--- make-git-repo.sh
+++ make-git-repo.sh
@@ -8,6 +8,7 @@ cd anduril2
 git init
 bzr fast-export --plain . | git fast-import && git reset HEAD --hard
 echo -e '.bzr\n.bzrignore\n.gitignore\n' > .gitignore ; if [[ ! -f .bzrignore ]] ; then echo -e '.bzrignore\n.git*' > .bzrignore; else echo '.git*' >> .bzrignore; fi
+#check if repo was created from an old git version
 if [[ "$(git rev-parse --abbrev-ref HEAD)" == "master" ]]
 then
 	git checkout -b main
@@ -16,10 +17,16 @@ fi
 git grep -l MISCHIEF_MANAGED | grep -v "${0}" | xargs -n1 sed -i 's/MISCHIEF_MANAGED/TRANS_RIGHTS_ARE_HUMAN_RIGHTS/g'
 git grep -l MISCHIEF_NOT_MANAGED | grep -v "${0}" | xargs -n1 sed -i 's/MISCHIEF_NOT_MANAGED/TRANS_RIGHTS_ARE_STILL_HUMAN_RIGHTS/g'
 git add -A ; git commit -m 'assert("trans rights!");'
-ls | grep -v -e 'ToyKeeper' -e 'bin' | xargs git rm -r
+while read line
+do
+	git rm -r "${line}"
+	#git doesn't automatically remove multiple levels deep dirs
+	[[ -d "${line}" && rm --one-file-system -r "${line}" ]]
+done <<< $(ls | grep -v -e 'ToyKeeper' -e 'bin' )
 cd ToyKeeper
-ls | grep -Ev -e '\.h$' -e 'spaghetti-monster' | xargs git rm -r
+ls | grep -Ev -e '\.h$' -e '\.c$' -e 'meta' -e 'COPYING' -e 'spaghetti-monster' -e 'anduril' | xargs git rm -r
 cd ..
 git mv ToyKeeper/* ./
 git rm -r ToyKeeper || true
-git add -A && git commit -m 'removed other fw'
+[[ -d ToyKeeper ]] && rmdir ToyKeeper
+git add -A && git commit -m 'move repo to root; remove other fw'
