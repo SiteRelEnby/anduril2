d7829637c91dc20d6d483da49f7727cc12cc2d05
SiteRelEnby <SiteRelEnby@localhost>
2023-03-05 16:31:11 -0600

    use a longer delay when using aux to indicate power on

--- spaghetti-monster/anduril/anduril.c
+++ spaghetti-monster/anduril/anduril.c
@@ -216,7 +216,9 @@ void setup() {
         // blink green at power-on to let user know power is connected
         #if defined(USE_AUX_RGB_LEDS)
         rgb_led_update(RGB_GREEN|RGB_HIGH, 0);
-        delay_4ms(10);
+        //delay_4ms(10);
+        //longer delay
+        delay_4ms(20);
         rgb_led_update(RGB_OFF, 0);
         #else
         // or just blink main emitter if we have no AUX LED
