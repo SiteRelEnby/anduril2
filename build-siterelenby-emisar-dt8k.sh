0929b18dbcdde0d62762ddd2e2d82264c3fde9a8
SiteRelEnby <SiteRelEnby@localhost>
2023-04-14 20:04:45 -0500

    move build scripts to subdir

--- build-siterelenby-emisar-dt8k.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-docker run --rm -v ${SCRIPTPATH}:/src -e COMMITHASH="$(git rev-parse --short HEAD)" -t anduril-builder:latest siterelenby-emisar-dt8k
-# For running from a subdir:
-# SCRIPTPATH was set above so it's known-good
-#BUILDPATH="$(cd ${SCRIPTPATH} && cd .. && pwd -P )"
-#docker run --rm -v ${BUILDPATH}:/src -e COMMITHASH="$(git rev-parse --short HEAD)" -i -t anduril-builder:latest noctigon-k1-siterelenby
