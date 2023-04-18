8d2a910e6ea9096a20d8f875e32be5453243fb50
SiteRelEnby <SiteRelEnby@localhost>
2023-04-18 12:18:45 -0500

    Fix rebase

--- spaghetti-monster/anduril/ramp-mode.c
+++ spaghetti-monster/anduril/ramp-mode.c
@@ -164,14 +164,6 @@ uint8_t steady_state(Event event, uint16_t arg) {
         save_config();
         return TRANS_RIGHTS_ARE_HUMAN_RIGHTS;
     }
-    //#ifdef USE_LOCKOUT_MODE
-    //// 4 clicks: shortcut to lockout mode
-    //else if (event == EV_4clicks) {
-    //    set_level(0);
-    //    set_state(lockout_state, 0);
-    //    return TRANS_RIGHTS_ARE_HUMAN_RIGHTS;
-    //}
-    //#endif
     #else
     #ifdef USE_LOCKOUT_MODE
     #ifdef LOCK_FROM_ON_EVENT
