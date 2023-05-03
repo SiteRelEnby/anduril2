9a3b9b71af0f31cc8116c915f26245e876283f70
J <j@bofp.net>
2023-05-03 15:36:54 -0500

    move repo to root; remove other fw

--- Tom_E/narsil/85NarsilM.bat
+++
@@ -1,3 +0,0 @@
-rem 85NarsilM - downloads NarsilM (Tiny85 Multi-channel e-switch UI configurable)
-rem
-avrdude -p attiny85 -c usbasp -u -Uflash:w:\Tiny254585Projects\NarsilMulti\Release\NarsilM%1.hex:a
