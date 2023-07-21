d17d04497416c82d1fae7c620a6e9933fbd9d075
SiteRelEnby <125829806+SiteRelEnby@users.noreply.github.com>
2023-07-21 02:02:14 -0500

    t1616 scripts

--- flash/flash-updi-t1616.sh
+++ flash/flash-updi-t1616.sh
@@ -1,2 +1,9 @@
+#! /usr/bin/env bash
 # for gchart's UPDI adapter
-avrdude -p attiny1616 -c serialupdi -Uflash:w:${1} || exit 1
+[[ -z "${PORT}" ]] && PORT="com5" #COM port will vary, this is just my default on my main box I use for development. TODO: *nix versions of these
+avrdude -p attiny1616 -c serialupdi -P ${PORT} -Uflash:w:${1}
+if [[ "${?}" != "0" ]]
+then
+	echo "avrdude failed. Make sure you have the right COM port (tried ${PORT}). # PORT=com8 ./${0}"
+	exit 1
+fi
--- flash/test-updi-t1616.sh
+++ flash/test-updi-t1616.sh
@@ -1,2 +1,9 @@
+#! /usr/bin/env bash
 # for gchart's UPDI adapter
-avrdude -p attiny1616 -c serialupdi || exit 1
+[[ -z "${PORT}" ]] && PORT="com5" #COM port will vary, this is just my default on my main box I use for development. TODO: *nix versions of these
+avrdude -p attiny1616 -c serialupdi -P ${PORT}
+if [[ "${?}" != "0" ]]
+then
+	echo "avrdude failed. Make sure you have the right COM port (tried ${PORT}). # PORT=com8 ./${0}"
+	exit 1
+fi
