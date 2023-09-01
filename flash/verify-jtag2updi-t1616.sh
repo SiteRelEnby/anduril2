695641170be68b7a8ff8daffddc82ad721dddc9d
SiteRelEnby <125829806+SiteRelEnby@users.noreply.github.com>
2023-09-01 13:06:50 -0500

    improve scripts

--- flash/verify-jtag2updi-t1616.sh
+++ flash/verify-jtag2updi-t1616.sh
@@ -1,2 +1,9 @@
+#! /usr/bin/env bash
 #for some rewireable UPDI adapters used for e.g. Sofirn Q8
-avrdude -p attiny1616 -c jtag2updi -Uflash:v:${1} || exit 1
+
+if [[ -z "${PORT}" ]]
+then
+	echo "Need a port. e.g.: $ PORT=COM6 ${0}" >&2
+	exit 1
+fi
+avrdude -p attiny1616 -c jtag2updi -P ${PORT} -Uflash:v:${1} || exit 1
