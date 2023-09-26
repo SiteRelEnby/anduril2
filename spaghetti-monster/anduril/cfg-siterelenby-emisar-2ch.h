f7d294ec85b77ef9ea5bedc5dd12684db297e218
SiteRelEnby <125829806+SiteRelEnby@users.noreply.github.com>
2023-09-25 20:46:22 -0500

    fix some warnings

--- spaghetti-monster/anduril/cfg-siterelenby-emisar-2ch.h
+++ spaghetti-monster/anduril/cfg-siterelenby-emisar-2ch.h
@@ -11,7 +11,9 @@
 //
 // Define some vars that are needed for the mod config to set stuff up properly
 #define NUM_MAIN_CHANNELS 2 //tell it this light has 2 'main' channels (for setting up shortcuts etc)
-#define USE_AUX_RGB_LEDS_WHILE_ON //this needs to be set to enable configurable RGB voltage
+#ifndef USE_AUX_RGB_LEDS_WHILE_ON
+  #define USE_AUX_RGB_LEDS_WHILE_ON //this needs to be set to enable configurable RGB voltage
+#endif
 #include "mod-config-siterelenby.h"
 #define USER_CONFIG_L0ADED //prevent defaults from loading
 
@@ -22,15 +24,28 @@
 #undef SIMPLE_UI_ACTIVE
 #define SIMPLE_UI_ACTIVE 0
 
+#undef RGB_LED_OFF_DEFAULT
 #define RGB_LED_OFF_DEFAULT 0x28 //high, rainbow
+#undef RGB_LED_LOCKOUT_DEFAULT
 #define RGB_LED_LOCKOUT_DEFAULT 0x18 //low, rainbow
 
-#define DEFAULT_BLINK_CHANNEL CM_AUXCYN
+#ifdef DEFAULT_BLINK_CHANNEL
+  #undef DEFAULT_BLINK_CHANNEL
+  #define DEFAULT_BLINK_CHANNEL CM_AUXCYN
+#endif
 
-#define USE_AUX_RGB_LEDS_WHILE_ON
-#define USE_AUX_RGB_LEDS_WHILE_ON_THRESHOLD_LOW 20
-#define USE_AUX_RGB_LEDS_WHILE_ON_THRESHOLD_HIGH 70
-#define BLINK_LOCK_REMINDER
+#ifndef USE_AUX_RGB_LEDS_WHILE_ON
+  #define USE_AUX_RGB_LEDS_WHILE_ON
+#endif
+#ifndef USE_AUX_RGB_LEDS_WHILE_ON_THRESHOLD_LOW
+  #define USE_AUX_RGB_LEDS_WHILE_ON_THRESHOLD_LOW 20
+#endif
+#ifndef USE_AUX_RGB_LEDS_WHILE_ON_THRESHOLD_HIGH
+  #define USE_AUX_RGB_LEDS_WHILE_ON_THRESHOLD_HIGH 70
+#endif
+#ifndef BLINK_LOCK_REMINDER
+  #define BLINK_LOCK_REMINDER
+#endif
 
 #ifdef LOCK_FROM_ON_EVENT
   #undef LOCK_FROM_ON_EVENT
@@ -41,5 +56,7 @@
 // B_RELEASE_T: activate when user lets go of button
 // B_TIMEOUT_T: activate when we're sure the user won't double-click
 // defaults are release on, timeout off
+#undef B_TIMING_ON
+#undef B_TIMING_OFF
 #define B_TIMING_ON B_TIMEOUT_T
 #define B_TIMING_OFF B_TIMEOUT_T
