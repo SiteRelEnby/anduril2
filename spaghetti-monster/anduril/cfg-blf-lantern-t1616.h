390db021c4721e4d606914d31af00561214f464d
SiteRelEnby <125829806+SiteRelEnby@users.noreply.github.com>
2023-10-15 18:26:49 -0500

    upgrade sp36, q8, and BLF LT1 t1616 variants to new API

--- spaghetti-monster/anduril/cfg-blf-lantern-t1616.h
+++ spaghetti-monster/anduril/cfg-blf-lantern-t1616.h
@@ -4,8 +4,15 @@
 #pragma once
 
 #define MODEL_NUMBER "0622"
-#include "hwdef-BLF_LT1-t1616.h"
+#include "hwdef-blf-lt1-t1616.h"
+
 // ATTINY: 1616
+
+#define DEFAULT_CHANNEL_MODE CM_BLEND
+#define DEFAULT_BLINK_CHANNEL CM_BLEND
+#define RAMP_SIZE 150
+#define USE_CHANNEL_MODE_ARGS
+#define USE_SET_LEVEL_GRADUALLY
 
 // the button lights up
 #define USE_INDICATOR_LED
@@ -15,10 +22,6 @@
 // lockout: blinking (3)
 #define INDICATOR_LED_DEFAULT_MODE ((3<<2) + 2)
 
-// the lantern has two PWM channels, but they drive different sets of emitters
-// (one channel for warm emitters, one channel for cold)
-// so enable a special ramping mode which changes tint instead of brightness
-#define USE_TINT_RAMPING
 // how much to increase total brightness at middle tint
 // (0 = 100% brightness, 64 = 200% brightness)
 //#define TINT_RAMPING_CORRECTION 26  // prototype, 140%
@@ -31,6 +34,7 @@
 // level_calc.py 1 150 7135 1 30 800
 #define RAMP_LENGTH 150
 #define PWM1_LEVELS 1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,11,11,12,13,13,14,15,15,16,17,18,18,19,20,21,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,43,44,45,46,48,49,50,51,53,54,56,57,58,60,61,63,64,66,67,69,70,72,74,75,77,79,80,82,84,85,87,89,91,93,95,97,98,100,102,104,106,108,111,113,115,117,119,121,124,126,128,130,133,135,137,140,142,145,147,150,152,155,157,160,163,165,168,171,173,176,179,182,185,188,190,193,196,199,202,205,209,212,215,218,221,224,228,231,234,238,241,245,248,251,255
+#define PWM_TOP 255
 #define MAX_1x7135 65
 #define HALFSPEED_LEVEL 14
 #define QUARTERSPEED_LEVEL 5
@@ -60,7 +64,7 @@
 #define USE_SOS_MODE
 #define USE_SOS_MODE_IN_BLINKY_GROUP
 
-// the sensor (attiny85) is nowhere near the emitters
+// the sensor (attiny1634) is nowhere near the emitters
 // so thermal regulation can't work
 #ifdef USE_THERMAL_REGULATION
 #undef USE_THERMAL_REGULATION
@@ -76,4 +80,13 @@
 // except the top... blink at the top
 #ifndef BLINK_AT_RAMP_CEIL
 #define BLINK_AT_RAMP_CEIL
+#endif
+
+// this light doesn't have RGB
+#ifdef USE_AUX_RGB_LEDS_WHILE_ON
+  #undef USE_AUX_RGB_LEDS_WHILE_ON
+#endif
+
+#ifndef USE_EXTENDED_SIMPLE_UI
+  #define USE_EXTENDED_SIMPLE_UI
 #endif
