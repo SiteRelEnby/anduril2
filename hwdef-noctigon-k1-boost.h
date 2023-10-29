bf86542cbeddbde984623ebc17aae42f81428b7a
Selene ToyKeeper <bzr@toykeeper.net>
2023-10-29 13:36:54 -0600

    converted noctigon-k1-boost to multi-channel with PWM+DSM (it's almost identical to noctigon-dm11-boost)

--- hwdef-noctigon-k1-boost.h
+++ hwdef-noctigon-k1-boost.h
@@ -29,26 +29,21 @@
  *
  * Main LED power uses one pin to turn the Opamp on/off,
  * and one pin to control Opamp power level.
- * All brightness control uses the power level pin, with 4 kHz 10-bit PWM.
+ * Regulated brightness control uses the power level pin, with PWM+DSM.
  * The on/off pin is only used to turn the main LED on and off,
  * not to change brightness.
  */
 
-#ifdef ATTINY
-#undef ATTINY
-#endif
 #define ATTINY 1634
 #include <avr/io.h>
 
-#ifndef HWDEF_C_FILE
-#define HWDEF_C_FILE hwdef-noctigon-k1-boost.c
-#endif
+#define HWDEF_C_FILE hwdef-noctigon-dm11-boost.c
 
 // allow using aux LEDs as extra channel modes
 #include "chan-rgbaux.h"
 
 // channel modes:
-// * 0. main LED
+// * 0. main LEDs
 // * 1+. aux RGB
 #define NUM_CHANNEL_MODES   (1 + NUM_RGB_AUX_CHANNEL_MODES)
 enum CHANNEL_MODES {
@@ -60,34 +55,52 @@ enum CHANNEL_MODES {
 
 // right-most bit first, modes are in fedcba9876543210 order
 #define CHANNEL_MODES_ENABLED 0b0000000000000001
+// no args
+//#define USE_CHANNEL_MODE_ARGS
+//#define CHANNEL_MODE_ARGS     0,0,0,0,0,0,0,0
 
-#define DEFAULT_BLINK_CHANNEL CM_MAIN
 
-#define PWM_CHANNELS 1
-#define PWM_BITS 10  // 0 to 1023 at 4 kHz, not 0 to 255 at 16 kHz
-#define PWM_GET PWM_GET16
-#define PWM_DATATYPE  uint16_t  // is used for PWM_TOPS (which goes way over 255)
+#define PWM_CHANNELS  1  // old, remove this
+
+#define PWM_BITS      16        // 0 to 32640 (0 to 255 PWM + 0 to 127 DSM) at constant kHz
+#define PWM_GET       PWM_GET16
+#define PWM_DATATYPE  uint16_t
 #define PWM_DATATYPE2 uint32_t  // only needs 32-bit if ramp values go over 255
-#define PWM1_DATATYPE uint16_t  // linear ramp
-//#define PWM_TOP_INIT  1023   // highest value used in top half of ramp
-#define PWM_CNT       TCNT1  // for dynamic PWM, reset phase
+#define PWM1_DATATYPE uint16_t  // 15-bit PWM+DSM ramp
 
-#define SWITCH_PIN   PA7    // pin 20
-#define SWITCH_PCINT PCINT7 // pin 20 pin change interrupt
-#define SWITCH_PCIE  PCIE0  // PCIE0 is for PCINT[7:0]
-#define SWITCH_PCMSK PCMSK0 // PCMSK0 is for PCINT[7:0]
-#define SWITCH_PORT  PINA   // PINA or PINB or PINC
+#define PWM_TOP       ICR1   // holds the TOP value for variable-resolution PWM
+#define PWM_TOP_INIT  255
+#define PWM_CNT       TCNT1  // for checking / resetting phase
+// (max is (255 << 7), because it's 8-bit PWM plus 7 bits of DSM)
+#define DSM_TOP       (255<<7) // 15-bit resolution leaves 1 bit for carry
 
-#define PWM1_PIN PB3        // pin 16, Opamp reference
-//#define CH1_PIN PB3        // pin 16, Opamp reference
-//#define PWM1_LVL OCR1A      // OCR1A is the output compare register for PB3
-#define CH1_PWM OCR1A      // OCR1A is the output compare register for PB3
+// timer interrupt for DSM
+#define DSM_vect     TIMER1_OVF_vect
+#define DSM_INTCTRL  TIMSK
+#define DSM_OVF_bm   (1<<TOIE1)
 
-#define CH1_ENABLE_PIN  PB0    // pin 19, Opamp power
-#define CH1_ENABLE_PORT PORTB  // control port for PB0
-#define CH1_ENABLE_PIN2  PC0    // pin 15, boost PMIC enable
-#define CH1_ENABLE_PORT2 PORTC  // control port for PC0
+#define DELAY_FACTOR 90  // less time in delay() because more time spent in interrupts
 
+// regulated channel
+uint16_t ch1_dsm_lvl;
+uint8_t ch1_pwm, ch1_dsm;
+#define CH1_PIN          PB3    // pin 16, Opamp reference
+#define CH1_PWM          OCR1A  // OCR1A is the output compare register for PB3
+
+#define CH1_ENABLE_PIN   PB0    // pin 19, Opamp power
+#define CH1_ENABLE_PORT  PORTB  // control port for PB0
+
+#define CH1_ENABLE_PIN2  PC0   // pin 15, boost PMIC enable
+#define CH1_ENABLE_PORT2 PORTC // control port for PC0
+
+// e-switch
+#define SWITCH_PIN   PA7     // pin 20
+#define SWITCH_PCINT PCINT7  // pin 20 pin change interrupt
+#define SWITCH_PCIE  PCIE0   // PCIE0 is for PCINT[7:0]
+#define SWITCH_PCMSK PCMSK0  // PCMSK0 is for PCINT[7:0]
+#define SWITCH_PORT  PINA    // PINA or PINB or PINC
+#define SWITCH_PUE   PUEA    // pullup group A
+#define PCINT_vect   PCINT0_vect  // ISR for PCINT[7:0]
 
 #define USE_VOLTAGE_DIVIDER  // use a dedicated pin, not VCC, because VCC input is flattened
 #define VOLTAGE_PIN PB1      // Pin 18 / PB1 / ADC6
@@ -108,7 +121,7 @@ enum CHANNEL_MODES {
 // Raw ADC readings at 4.4V and 2.2V
 // calibrate the voltage readout here
 // estimated / calculated values are:
-//   (voltage - D1) * (R2/(R2+R1) * 256 / 1.1)
+//   (voltage - D1) * (R2/(R2+R1) * 1024 / 1.1)
 // D1, R1, R2 = 0, 330, 100
 #ifndef ADC_44
 //#define ADC_44 981  // raw value at 4.40V
@@ -127,40 +140,49 @@ enum CHANNEL_MODES {
 #define AUXLED_RGB_DDR  DDRA   // DDRA or DDRB or DDRC
 #define AUXLED_RGB_PUE  PUEA   // PUEA or PUEB or PUEC
 
-// with so many pins, doing this all with #ifdefs gets awkward...
-// ... so just hardcode it in each hwdef file instead
 inline void hwdef_setup() {
-  // enable output ports
-  // boost PMIC on/off
-  DDRC = (1 << CH1_ENABLE_PIN2);
-  // Opamp level and Opamp on/off
-  DDRB = (1 << PWM1_PIN)
-       | (1 << CH1_ENABLE_PIN);
-  // aux R/G/B
-  DDRA = (1 << AUXLED_R_PIN)
-       | (1 << AUXLED_G_PIN)
-       | (1 << AUXLED_B_PIN)
-       ;
+    // enable output ports
+    // boost PMIC on/off
+    DDRC = (1 << CH1_ENABLE_PIN2);
+    // Opamp level and Opamp on/off
+    DDRB = (1 << CH1_PIN)
+         | (1 << CH1_ENABLE_PIN);
+    // aux R/G/B
+    DDRA = (1 << AUXLED_R_PIN)
+         | (1 << AUXLED_G_PIN)
+         | (1 << AUXLED_B_PIN)
+         ;
 
-  // configure PWM
-  // Setup PWM. F_pwm = F_clkio / 2 / N / TOP, where N = prescale factor, TOP = top of counter
-  // pre-scale for timer: N = 1
-  // WGM1[3:0]: 0,0,1,1: PWM, Phase Correct, 10-bit (DS table 12-5)
-  // CS1[2:0]:    0,0,1: clk/1 (No prescaling) (DS table 12-6)
-  // COM1A[1:0]:    1,0: PWM OC1A in the normal direction (DS table 12-4)
-  // COM1B[1:0]:    0,0: PWM OC1B disabled (DS table 12-4)
-  TCCR1A  = (1<<WGM11)  | (1<<WGM10)   // 10-bit (TOP=0x03FF) (DS table 12-5)
-          | (1<<COM1A1) | (0<<COM1A0)  // PWM 1A in normal direction (DS table 12-4)
-          | (0<<COM1B1) | (0<<COM1B0)  // PWM 1B in normal direction (DS table 12-4)
-          ;
-  TCCR1B  = (0<<CS12)   | (0<<CS11) | (1<<CS10)  // clk/1 (no prescaling) (DS table 12-6)
-          | (0<<WGM13)  | (0<<WGM12)  // phase-correct PWM (DS table 12-5)
-          ;
+    // configure PWM
+    // Setup PWM. F_pwm = F_clkio / 2 / N / TOP, where N = prescale factor, TOP = top of counter
+    // pre-scale for timer: N = 1
+    // PWM for both channels
+    // WGM1[3:0]: 1,0,1,0: PWM, Phase Correct, adjustable (DS table 12-5)
+    // WGM1[3:0]: 1,1,1,0: PWM, Fast, adjustable (DS table 12-5)
+    // CS1[2:0]:    0,0,1: clk/1 (No prescaling) (DS table 12-6)
+    // COM1A[1:0]:    1,0: PWM OC1A in the normal direction (DS table 12-4)
+    // COM1B[1:0]:    1,0: PWM OC1B in the normal direction (DS table 12-4)
+    TCCR1A = (1<<WGM11)  | (0<<WGM10)   // adjustable PWM (TOP=ICR1) (DS table 12-5)
+           | (1<<COM1A1) | (0<<COM1A0)  // PWM 1A in normal direction (DS table 12-4)
+           | (1<<COM1B1) | (0<<COM1B0)  // PWM 1B in normal direction (DS table 12-4)
+           ;
+    TCCR1B = (0<<CS12)   | (0<<CS11) | (1<<CS10)  // clk/1 (no prescaling) (DS table 12-6)
+           //| (1<<WGM13)  | (1<<WGM12)  // fast adjustable PWM (DS table 12-5)
+           | (1<<WGM13)  | (0<<WGM12)  // phase-correct adjustable PWM (DS table 12-5)
+           ;
 
-  // set up e-switch
-  PUEA = (1 << SWITCH_PIN);  // pull-up for e-switch
-  SWITCH_PCMSK = (1 << SWITCH_PCINT);  // enable pin change interrupt
+    // set PWM resolution
+    PWM_TOP = PWM_TOP_INIT;
+
+    // set up interrupt for delta-sigma modulation
+    // (moved to hwdef.c functions so it can be enabled/disabled based on ramp level)
+    //DSM_INTCTRL |= DSM_OVF_bm;  // interrupt once for each timer cycle
+
+    // set up e-switch
+    SWITCH_PUE = (1 << SWITCH_PIN);  // pull-up for e-switch
+    SWITCH_PCMSK = (1 << SWITCH_PCINT);  // enable pin change interrupt
 }
+
 
 #define LAYOUT_DEFINED
 
